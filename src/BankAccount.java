5d08b5eea0376348d8cb9ae08e8130cce091eb84
Vlad Sablin <vlads-vlads@inbox.lv>
2024-03-28 15:22:13 +0200

    Serialize/deserialize methods are added + tranfer function bug fixxed

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,5 +1,10 @@
-public class BankAccount {
-    private int id;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.*;
+
+public class BankAccount implements Serializable {
+    private final int id;
     private double balance;
 
     public BankAccount(int id, double balance) {
@@ -11,6 +16,10 @@ public class BankAccount {
         return id;
     }
 
+    public double getBalance() {
+        return balance;
+    }
+
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
@@ -18,15 +27,19 @@ public class BankAccount {
         } else {
             System.out.println("Invalid amount for deposit.");
         }
+        String transactionDetails = "Account id: " + getId() + " | Deposit: Amount $" + amount + " | Balance $" + getBalance();
+        writeTransactionToFile(transactionDetails);
     }
 
     public void withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
+        if (amount > 0 && amount <= getBalance()) {
             balance -= amount;
             System.out.println("Withdrawal successful.");
         } else {
             System.out.println("Invalid amount for withdrawal or insufficient balance.");
         }
+        String transactionDetails = "Withdrawal: Amount $" + amount + " | Balance $" + getBalance();
+        writeTransactionToFile(transactionDetails);
     }
 
     public void transfer(BankAccount recipient, double amount) {
@@ -37,9 +50,47 @@ public class BankAccount {
         } else {
             System.out.println("Invalid amount for transfer or insufficient balance.");
         }
+        String senderTransaction = "Transfer to Account " + recipient.getId() + ": Amount $" + amount + " | Balance $" + getBalance();
+        writeTransactionToFile(senderTransaction);
+
+
+        String recipientTransaction = "Transfer from Account " + id + ": Amount $" + amount + " | Balance $" + recipient.getBalance();
+        recipient.writeTransactionToFile(recipientTransaction);
     }
 
     public void printBalance() {
-        System.out.println("Balance: $" + balance);
+        System.out.println("Balance: $" + getBalance());
+    }
+
+    public void writeTransactionToFile(String transactionDetails) {
+        String filename = "transaction_records.txt";
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename, true))) {
+            writer.println(transactionDetails);
+        } catch (IOException e) {
+            System.err.println("Error writing transaction to file: " + e.getMessage());
+        }
+    }
+
+    public void serialize(String folderPath) {
+        String filename = folderPath + File.separator + "account_" + id + ".ser";
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
+            out.writeObject(this);
+            System.out.println("BankAccount serialized successfully to " + filename);
+        } catch (IOException e) {
+            System.err.println("Error serializing BankAccount: " + e.getMessage());
+        }
+    }
+
+    public static BankAccount deserialize(String filename) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
+            BankAccount account = (BankAccount) in.readObject();
+            System.out.println("BankAccount deserialized successfully from " + filename);
+            return account;
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error deserializing BankAccount: " + e.getMessage());
+            return null;
+        }
     }
 }
+
+
